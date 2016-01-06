77fa5d894f35ddfecada86a07466d06821ff01c0
suttang <suttang@gmail.com>
2016-01-06 16:31:15 +0900

    宇宙的な

--- src/assets/coffee/modules/screen.js
+++ src/assets/coffee/modules/screen.js
@@ -1,51 +1,10 @@
-"use strict";
+
 
 const contents = '#contents';
-
-let wh = 0;
-let ww = 0;
-let contentOffsets = [];
-let current = 0;
 
 export class Screen {
 
   static init() {
-    $('html').addClass('init');
-
-    wh = $(window).height();
-    ww = $(window).width();
-
-    $(contents).find('> .wamw').height(wh);
-
-    $(window).on('scroll', this.scrollHandler.bind(this));
-
-    $('html').addClass('anim');
-    // setTimeout(() => {
-    //   this.show();
-    // }, 2500);
-  }
-
-  static scrollHandler(event) {
-    if ($(window).scrollTop() > 500) {
-      // console.log($(window).scrollTop());
-      // console.log(this);
-      this.show();
-    } else {
-      this.black();
-    }
-  }
-
-  static show() {
-    // $(contents).find('> section').show();
-    $('html')
-      .removeClass('init')
-      .addClass('show');
-  }
-
-  static black() {
-    $('html')
-      .removeClass('show')
-      .addClass('init');
   }
 
 }
